ef3e072ea952a2cf6cc500f7f1262d8930be4557
Tirth Patel <tirthpatel49@gmail.com>
2016-10-11 22:48:41 +0530

    Disabling Sepolicy Perms for Now.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,10 +118,10 @@ TARGET_LDPRELOAD += libxlog.so
 BOARD_SEPOLICY_DIRS := \
        device/lenovo/Tab2A710F/sepolicy
 
-BOARD_SEPOLICY_UNION := \
-        device.te \
-        netd.te \
-        system.te \
-        file_contexts
+#BOARD_SEPOLICY_UNION := \
+#        device.te \
+#        netd.te \
+#        system.te \
+#        file_contexts
        
        
